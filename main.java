fbde066ad88bd2a1e25997f5cc4697acd308a9f0
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-19 11:47:06 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+//file name should be as  fact.java
+package Class;
+
+public class fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fact_Class n=new fact_Class();
+		n.get();
+		n=null;
+	}
+
+}
